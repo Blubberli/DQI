52227fcb0f363271f31cc9e33791750bce650dea
Blubberli <neele.witte@uni-tuebingen.de>
2021-12-08 18:26:30 +0100

    code to train models on deliberative quality

---
+++ prepare_europolis.py
@@ -0,0 +1,144 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+from sklearn.model_selection import train_test_split, StratifiedKFold
+import random
+
+random.seed(41)
+# merge the classes and give them meaningful names(?)
+# plot the class distribution(?)
+# have german, english, original language text, all preprocessed, have unique ID, have argQuality Scores
+# create a stratified split for each quality dimension
+
+label2textencoding = {
+    "jlev": {
+        0: 'no justification',
+        1: 'inferior justification',
+        2: 'qualified justification',
+        3: 'sophisticated (broad)',
+        4: 'sophisticated (depth)'
+
+    },
+    "jcon": {
+        0: 'own country',
+        1: 'no reference',
+        2: 'reference to common good (EU)',
+        3: 'reference to common good (solidarty)'
+
+    },
+    "resp_gr": {
+        0: 'disrespectful',
+        1: 'implicit respect',
+        2: 'balanced respect',
+        3: 'explicit respect'
+    },
+    "int1": {
+        0: 'no reference',
+        1: 'negative reference',
+        2: 'neutral reference',
+        3: 'positive reference'
+
+    }
+}
+
+text2new_label = {
+    "jlev": {
+        'no justification': 0,
+        'inferior justification': 1,
+        'qualified justification': 2,
+        'sophisticated (broad)': 3,
+        'sophisticated (depth)': 3
+
+    },
+    "jcon": {
+        'own country': 0,
+        'no reference': 1,
+        'reference to common good (EU)': 2,
+        'reference to common good (solidarty)': 2
+
+    },
+    "resp_gr": {
+        'disrespectful': 0,
+        'implicit respect': 1,
+        'balanced respect': None,
+        'explicit respect': 2
+    },
+    "int1": {
+        'no reference': 0,
+        'negative reference': 1,
+        'neutral reference': 2,
+        'positive reference': 3
+
+    }
+
+}
+
+
+def convert_label_column(dimension, label_list):
+    """Convert the old labels to the new labels and return the new label list"""
+    text_labels = [label2textencoding[dimension][l] for l in label_list]
+    new_labels = [text2new_label[dimension][text_label] for text_label in text_labels]
+    return new_labels
+
+
+def create_europolis_with_merged_labels():
+    """Create the new europolis dataframe with merged labels"""
+    data = pd.read_csv("/Users/johannesfalk/PycharmProjects/DQIReReloaded/data/europolis_dqi_with_terciles.csv",
+                       sep="\t")
+    data["post_length"] = [len(comment.split(" ")) for comment in data["cleaned_comment"].values]
+    data = data[data.post_length >= 15]
+    print(len(data))
+    data = data.rename(columns={"label": "jlev"})
+    dimensions = ["jlev", "jcon", "resp_gr", "int1"]
+    new_label_cols = []
+    for dim in dimensions:
+        label_col = data[dim].values
+        new_label_col = convert_label_column(dimension=dim, label_list=label_col)
+        new_label_cols.append(new_label_col)
+    data = data.rename(columns={"jlev": "jlev_old_labels", "jcon": "jcon_old_labels", "resp_gr": "resp_gr_old_labels",
+                                "int1": "int1_old_labels"})
+    data["jlev"] = new_label_cols[0]
+    data["jcon"] = new_label_cols[1]
+    data["resp_gr"] = new_label_cols[2]
+    data["int1"] = new_label_cols[3]
+    data.to_csv("data/europolis_newDQI.csv", index=False, sep="\t")
+
+
+def plot_class_distributions():
+    """Read the whole dataset and plot label distribution for each quality dimension"""
+    data = pd.read_csv("data/europolis_newDQI.csv", sep="\t")
+    dimensions = ["jlev", "jcon", "resp_gr", "int1"]
+    for dim in dimensions:
+        counts = pd.crosstab(index=data[dim], columns='count')
+        counts.plot.bar()
+        plt.savefig(
+            "/Users/johannesfalk/PycharmProjects/DQIReReloaded/plots/original_class_distribution/%s.png" % dim)
+
+
+def create_stratified_split(quality_dim, output_dir):
+    """Given a label and the output path, generate a 5fold (stratified) split"""
+    data = pd.read_csv("data/europolis_newDQI.csv", sep="\t")
+    # drop where label is None
+    data = data[data[quality_dim].notna()]
+    kfold = StratifiedKFold(n_splits=5, shuffle=True, random_state=42)
+
+    counter = 0
+    for train, test in kfold.split(data, data[quality_dim]):
+        train_set, test_set = data.loc[train], data.loc[test]
+        train_set, val_set = train_test_split(train_set, test_size=0.25)
+
+        train_set.to_csv("%s/split%d/train.csv" % (output_dir, counter),
+                         sep="\t", index=False)
+        val_set.to_csv("%s/split%d/val.csv" % (output_dir, counter), sep="\t",
+                       index=False)
+        test_set.to_csv("%s/split%d/test.csv" % (output_dir, counter),
+                        sep="\t", index=False)
+        counter += 1
+
+        print("train: %d, val: %d, test:%d" % (len(train_set), len(val_set), len(test_set)))
+        print('train: %.2f, val:%.2f, test: %.2f' % (
+            len(train_set) / len(data), len(val_set) / len(data), len(test_set) / len(data)))
+
+
+if __name__ == '__main__':
+    # plot_class_distributions()
+    create_stratified_split("resp_gr", "data/5foldStratified/resp_gr")
---
+++ training.py
@@ -0,0 +1,156 @@
+import torch
+import wandb
+from pathlib import Path
+from logging import get_name_with_hyperparams, reset_wandb_env
+from args import parse_arguments
+from transformers import AutoTokenizer, AutoModelForSequenceClassification, EvalPrediction, Trainer
+from sklearn.metrics import accuracy_score, precision_recall_fscore_support, classification_report
+from transformers import set_seed
+from transformers.integrations import WandbCallback
+import torch.nn.functional as F
+from seq_with_feats import RobertaWithFeats
+from transformers import RobertaConfig
+from data import EuropolisDataset
+import numpy as np
+
+# from pytorch_lightning.callbacks import ModelCheckpoint
+# from pytorch_lightning.trainer import Trainer
+
+# set seed to 42 for reproducibility
+set_seed(42)
+
+# check for GPUs or CPU
+if torch.cuda.is_available():
+    device = torch.device("cuda")
+    print('GPU in use:')
+else:
+    print('using the CPU')
+    device = torch.device("cpu")
+
+
+def run_train_with_trainer(train_data, dev_data, test_data, data_args, model_args, training_args,
+                           fold_id, test_csv):
+    # initialize classification model
+    # general = name with important hyperparams
+    # split_run_name = adding the split ID to the name
+    general_name, split_run_name = get_name_with_hyperparams(data_args=data_args, model_args=model_args,
+                                                             training_args=training_args,
+                                                             fold_id=fold_id)
+    # create a directory to store the results
+    general_dir = training_args.output_dir
+    split_dir = Path(training_args.output_dir + "/" + split_run_name)
+    if not split_dir.exists():
+        split_dir.mkdir()
+    # start wanDB
+    reset_wandb_env()
+    wandb_run = wandb.init(project=training_args.project_name,
+                           group=general_name,
+                           name=split_run_name,
+                           reinit=True,
+                           settings=wandb.Settings(start_method="fork"))
+    # create WanDB callback
+    wandb_callback = WandbCallback()
+    # create early stopping callback
+
+    # set the output directory to the run-specific directory to store the models there
+    training_args.output_dir = split_dir
+    trainer = Trainer(
+        model=model,
+        args=training_args,
+        train_dataset=train_data,
+        eval_dataset=dev_data,
+        compute_metrics=compute_metrics,
+        callbacks=[wandb_callback]
+    )
+
+    initial_train_results = trainer.evaluate(train_data)
+
+    # train with trainer
+    trainer.train()
+
+    train_results = trainer.evaluate(train_data)
+
+    # evaluate on dev set
+    dev_results = trainer.evaluate(dev_data)
+
+    test_result = trainer.evaluate(test_data)
+    dev_report = dev_results["eval_report"]
+    test_report = test_result["eval_report"]
+
+    wandb_run.finish()
+    prediction_output = trainer.predict(test_data)
+    #            probs = F.softmax(out, dim=-1)
+
+    test_csv['predictions'] = F.softmax(torch.tensor(prediction_output.predictions), dim=-1).tolist()
+    test_csv.to_csv(f'{str(split_dir)}/test_df_with_predictions.csv', index=False, sep="\t")
+    training_args.output_dir = general_dir
+
+    return dev_results, test_result
+
+
+def compute_metrics(pred: EvalPrediction):
+    labels = pred.label_ids
+    preds = np.argmax(pred.predictions, axis=1).flatten()
+    precision, recall, macro_f1, _ = precision_recall_fscore_support(y_true=labels, y_pred=preds, average='macro')
+    accuracy = accuracy_score(y_true=labels, y_pred=preds)
+    report = classification_report(y_true=labels, y_pred=preds)
+    results = {
+        'accuracy': accuracy,
+        'macro_f1': macro_f1,
+        'precision': precision,
+        'recall': recall,
+        "report": report
+    }
+    return results
+
+
+def save_results(output_dir, report, filename):
+    with open(output_dir + "/%s" % filename, "w") as f:
+        f.write(str(report))
+    print("results saved in %s" % filename)
+
+
+if __name__ == '__main__':
+    # read in arguments
+    # model args: all classification details
+    # data args: path to dataset etc.
+    # training args: learning rate, optimizer etc.
+    model_args, data_args, training_args = parse_arguments()
+    print("model is trained for %s " % data_args.quality_dim)
+    # init tokenizer
+    tokenizer = AutoTokenizer.from_pretrained(model_args.model_name_or_path)
+    if model_args.use_feats:
+        config = RobertaConfig.from_pretrained(
+            'roberta-base',
+            num_labels=model_args.labels_num,  # The number of output labels--3 for classification.
+        )
+        # Pass in the number of numerical features.
+        config.numerical_feat_dim = 4
+
+        # Pass in the size of the text embedding.
+        # The text feature dimension is the "hidden_size" parameter which
+        # comes from RobertaConfig. The length is 768 in ROBERTA-base (and most other BERT
+        # models).
+        config.text_feat_dim = config.hidden_size  # 768
+        # load the adapted model with the modified config
+        model = RobertaWithFeats(roberta_config=config)
+    else:
+        # use a normal seq classification model without features
+        model = AutoModelForSequenceClassification.from_pretrained(model_args.model_name_or_path)
+
+    for i in range(0, 5):
+        # create a training, dev and test dataset
+        train = EuropolisDataset(path_to_dataset=data_args.data_dir + "/split%i/train.csv" % i,
+                                 label=data_args.quality_dim, tokenizer=tokenizer, text_col=data_args.text_col)
+        dev = EuropolisDataset(path_to_dataset=data_args.data_dir + "/split%i/val.csv" % i,
+                               label=data_args.quality_dim, tokenizer=tokenizer, text_col=data_args.text_col)
+        test = EuropolisDataset(path_to_dataset=data_args.data_dir + "/split%i/test.csv" % i,
+                                label=data_args.quality_dim, tokenizer=tokenizer, text_col=data_args.text_col)
+
+        dev_results, test_results = run_train_with_trainer(train_data=train,
+                                                           test_data=dev,
+                                                           dev_data=test,
+                                                           data_args=data_args,
+                                                           model_args=model_args,
+                                                           training_args=training_args,
+                                                           fold_id=str(i), test_csv=test)
